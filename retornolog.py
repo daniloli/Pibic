168a3f8ef5b15488c852abae6637246922cea248
daniloli <41523315+daniloli@users.noreply.github.com>
2018-09-26 23:19:27 -0300

    Update retornolog.py

--- retornolog.py
+++ retornolog.py
@@ -1,8 +1,33 @@
 import seaborn as sns
+import csv
+import urllib2
+from math import *
+from matplotlib.pylab import *
+date=[]
+close=[]
+high=[]
+low=[]
+opn=[]
+volume=[]
+ 
+inp=raw_input('digite o nome do arquivo:')
+f=open('%s' %inp ,'a+')
+i=0
+for line in f:
+    if i==0:
+        i+=1
+        continue
+    x=line.split(',')
+    date.append((x[0]))
+    close.append((x[1]))
+    high.append(float(x[2]))
+    low.append(float(x[3]))
+    opn.append((x[4]))
+    volume.append((x[5]))
 
 
 d=input('digite o valor da janela(maior que 0):')
-serie=raw_input('digite a serie sobre a qual deseja manipular(open, close, high, low, volume:)')
+serie=raw_input('digite a serie sobre a qual deseja manipular(open, close, high, low, volume):')
 while (serie != 'open'and serie != 'close'and serie != 'high'and serie != 'low' and serie != 'volume'):
     print 'serie nao definida!'
     serie=raw_input('digite a serie sobre a qual deseja manipular(open, close, high, low, volume:)')
@@ -10,24 +35,26 @@ if serie == 'open':
     serie='opn'
 serie='serie='+serie
 exec serie
+
+
+
 Rt=[]
 for i in range (d,len(serie)):
     Rt.append(log(serie[i]/serie[i-d]))
 squares=list(map(lambda x:pow(x,2),Rt))
 soma=[]
+theil=[]
+for i in range(len(serie)-d):
+    Mu=mean(serie[i:i+d])
+    janela=[]
+    for j in range(d):
+        s=(serie[i+j]/Mu)*log(serie[i+j]/Mu)
+        janela.append(s)
+    theil.append((mean (janela)))
+    
 for i in range (len(Rt)-d):
     soma.append(sum(squares[i:i+d])/d)
     
-    
-theil=[]
-for i in range(len(serie)-d):
-    Mu=sum(serie[i:i+d])/d
-    janela=[]
-    for j in range(d):
-        s=(serie[i+d]/Mu)+log(serie[i+d]/Mu)
-        janela.append(s)
-    theil.append((sum (janela)/d))
-
 figure(1)
 plot(Rt)
 xlabel('t')
